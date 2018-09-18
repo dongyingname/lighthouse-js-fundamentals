79e6a1a053148c1a464f9295b2b3a8b3629aeb92
Ying Dong <dongyingname@yahoo.com>
2018-09-18 20:10:36 +0000

    this time i added many .js files that I've worked on

---
+++ howManyHundreds.js
@@ -0,0 +1,8 @@
+function howManyHundreds(num){
+  return (num - num % 100) / 100;
+}
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
